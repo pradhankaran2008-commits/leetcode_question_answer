d26cecd8917a4dffcf4a4b2551df0c6583a1bdef
pradhankaran2008-commits <pradhankaran2008@gmail.com>
2026-09-22 04:41:02 +0530

    Time: 0 ms (100%), Space: 9.5 MB (69.88%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int secondHighest(string s) {
         int a[10] = {0};
-
         for(int i = 0; i < s.length(); i++) {
             if(s[i] >= '0' && s[i] <= '9') {
                 int n = s[i] - 48;
                 a[n] = 1;
             }
         }
-
         int count = 0;
-
         for(int i = 9; i >= 0; i--) {
             if(a[i] == 1) {
                 count++;
-
                 if(count == 2)
                     return i;
             }
         }
-
         return -1;
     }
 
